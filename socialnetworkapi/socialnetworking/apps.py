ca62c562a1e83232e204db491cdc1b9cf25a82af
MinkNhat <nquyenminhnhat0503@gmail.com>
2025-02-25 00:26:56 +0700

    update

--- socialnetworkapi/socialnetworking/apps.py
+++ socialnetworkapi/socialnetworking/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SocialnetworkingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'socialnetworking'
+    name = 'socialnetworkapi.socialnetworking'
